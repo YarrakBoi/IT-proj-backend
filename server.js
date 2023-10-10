95697ba89f8f39bdd51677b65aa2425a33ab79b7
YarrakBoi <kcaminatra@gmail.com>
2023-10-10 17:27:14 +1100

    testing

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ app.use(cors({
     origin: 'https://timetable-itproj-e2ce1e31bd73.herokuapp.com/'
 })); 
 app.use("/api/events", eventRoute);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 // Connect to database
 connectToDb();
