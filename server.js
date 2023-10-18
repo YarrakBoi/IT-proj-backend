078bb9ef9dfa5ace670df1d17be132bee8ada822
YarrakBoi <kcaminatra@gmail.com>
2023-10-18 15:22:14 +1100

    added originin link

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cors()); 
 app.use("/api/events", eventRoute);
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", 'https://timetable-it-11e90cc8646f.herokuapp.com');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
